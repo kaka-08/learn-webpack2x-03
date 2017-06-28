3a945062488218f33485dcdc22526cea9f7bdde3
kaka_08 <602993561@qq.com>
2017-06-28 14:51:49 +0800

    webpack配置插件

---
+++ README.md
@@ -0,0 +1,10 @@
+webpack引入模块
+====================================
+学习 Html-webpack-plugin插件 
+------------------------------------
+
+
+#1 执行 npm init 命令  初始化 package.json   
+#2 按照目录结构创建对应的文件 
+#3 touch webpack.config.js 文件 并配置
+#4 执行 webpack 命令  会发现在 dist 文件夹下编译生成了 webpack 的 output 文件
---
+++ webpack.config.js
@@ -0,0 +1,43 @@
+var path = require("path");
+var HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  module:{
+  	rules:[
+      {
+      	 test: /\.css$/,  //载入css
+         use: [
+           'style-loader',
+           'css-loader'
+          ]
+      },
+       {
+         test: /\.(png|svg|jpg|gif)$/, //载入图像
+         use: [
+           'file-loader'
+         ]
+       },
+       {
+        test: /\.(woff|woff2|eot|ttf|otf)$/, //加载字体
+        use: [
+           'file-loader'
+         ]
+       }
+  	]
+  },
+  //devtool: "cheap-eval-source-map",  //跟踪JavaScript错误异常 知识点在 source-map
+  devtool: "inline-source-map",
+  plugins: [
+    //该插件自动生成 目标 html文件 
+    new HtmlWebpackPlugin({
+      title: 'Output Management',
+      favicon: './favicon.ico',
+      template: './dist/aaa.html'  //以  aaa.html 为模板，可以不配置这一项，如果配置了这一项，则上面的title无效
+    })
+  ]
+};
